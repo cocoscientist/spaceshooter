d8a9017b9d5dafe291f63c533646d9747e7b5359
Yash <ypt7895@gmail.com>
2024-06-16 16:56:19 +0530

    added asset loader with player asset

---
+++ assets/assets.go
@@ -0,0 +1,28 @@
+package assets
+
+import (
+	"embed"
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed *
+var assets embed.FS
+
+var PlayerSprite = mustLoadImage("player.png")
+
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	return ebiten.NewImageFromImage(img)
+}
