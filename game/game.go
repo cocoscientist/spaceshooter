865d8a57fb13223e22e33e2b073304638be68067
Yash <ypt7895@gmail.com>
2024-06-16 16:56:58 +0530

    added player

--- game/game.go
+++ game/game.go
@@ -1,25 +1,31 @@
 package game
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 const (
 	screenWidth  = 800
 	screenHeight = 600
 )
 
-type Game struct{}
+type Game struct {
+	player *Player
+}
 
 func NewGame() *Game {
 	g := &Game{}
+	g.player = NewPlayer()
 	return g
 }
 
 func (g *Game) Update() error {
+	g.player.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	g.player.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
---
+++ game/player.go
@@ -0,0 +1,43 @@
+package game
+
+import (
+	"spaceshooter/assets"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	speedPerSecond = 350
+)
+
+type Player struct {
+	position Vector
+	sprite   *ebiten.Image
+}
+
+func NewPlayer() *Player {
+	sprite := assets.PlayerSprite
+	position := Vector{
+		X: 350,
+		Y: 500,
+	}
+	return &Player{
+		sprite:   sprite,
+		position: position,
+	}
+}
+
+func (p *Player) Update() {
+	speed := speedPerSecond / float64(ebiten.TPS())
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.position.X -= speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.position.X += speed
+	}
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(p.position.X, p.position.Y)
+	screen.DrawImage(p.sprite, op)
+}
