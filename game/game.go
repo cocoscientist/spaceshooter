82b39e8c12d8802da92bf198b7ecac9ecb98f75f
Yash <ypt7895@gmail.com>
2024-06-17 02:53:10 +0530

    timed meteor spawn

--- game/game.go
+++ game/game.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"time"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -10,30 +12,44 @@ const (
 )
 
 type Game struct {
-	player *Player
-	bg     *Background
-	meteor *Meteor
+	player           *Player
+	bg               *Background
+	meteorSpawnTimer *Timer
+	meteors          []*Meteor
 }
 
 func NewGame() *Game {
 	g := &Game{}
+	g.meteorSpawnTimer = NewTimer(2 * time.Second)
 	g.player = NewPlayer()
 	g.bg = NewBackground()
-	g.meteor = NewMeteor()
 	return g
 }
 
 func (g *Game) Update() error {
 	g.bg.Update()
 	g.player.Update()
-	g.meteor.Update()
+	g.meteorSpawnTimer.Update()
+	if g.meteorSpawnTimer.IsReady() {
+		g.meteorSpawnTimer.Reset()
+		m := NewMeteor()
+		g.meteors = append(g.meteors, m)
+	}
+	for i, meteor := range g.meteors {
+		meteor.Update()
+		if meteor.position.Y > 480 {
+			g.meteors = append(g.meteors[:i], g.meteors[i+1:]...)
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.bg.Draw(screen)
 	g.player.Draw(screen)
-	g.meteor.Draw(screen)
+	for _, meteor := range g.meteors {
+		meteor.Draw(screen)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
