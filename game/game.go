f0f93908a0544c2a880ead996c38f6c8ce3c21bf
Yash <ypt7895@gmail.com>
2024-06-17 14:26:12 +0530

    added laser to game

--- game/game.go
+++ game/game.go
@@ -16,11 +16,14 @@ type Game struct {
 	bg               *Background
 	meteorSpawnTimer *Timer
 	meteors          []*Meteor
+	lasers           []*Laser
+	laserTimer       *Timer
 }
 
 func NewGame() *Game {
 	g := &Game{}
 	g.meteorSpawnTimer = NewTimer(2 * time.Second)
+	g.laserTimer = NewTimer(1 * time.Second)
 	g.player = NewPlayer()
 	g.bg = NewBackground()
 	return g
@@ -35,10 +38,22 @@ func (g *Game) Update() error {
 		m := NewMeteor()
 		g.meteors = append(g.meteors, m)
 	}
+	g.laserTimer.Update()
+	if g.laserTimer.IsReady() && ebiten.IsKeyPressed(ebiten.KeySpace) {
+		g.laserTimer.Reset()
+		l := NewLaser(g.player.position.X + float64(g.player.sprite.Bounds().Dx())/2)
+		g.lasers = append(g.lasers, l)
+	}
 	for i, meteor := range g.meteors {
 		meteor.Update()
 		if meteor.position.Y > 480 {
 			g.meteors = append(g.meteors[:i], g.meteors[i+1:]...)
+		}
+	}
+	for i, laser := range g.lasers {
+		laser.Update()
+		if laser.position.Y < 0 {
+			g.lasers = append(g.lasers[:i], g.lasers[i+1:]...)
 		}
 	}
 	return nil
@@ -49,6 +64,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 	for _, meteor := range g.meteors {
 		meteor.Draw(screen)
+	}
+	for _, laser := range g.lasers {
+		laser.Draw(screen)
 	}
 }
 
---
+++ game/laser.go
@@ -0,0 +1,33 @@
+package game
+
+import (
+	"spaceshooter/assets"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Laser struct {
+	position Vector
+	sprite   *ebiten.Image
+}
+
+func NewLaser(x float64) *Laser {
+	sprite := assets.LaserSprite
+	return &Laser{
+		position: Vector{
+			X: x - float64(sprite.Bounds().Dx())/2,
+			Y: 500 - float64(sprite.Bounds().Dy()),
+		},
+		sprite: sprite,
+	}
+}
+
+func (l *Laser) Update() {
+	l.position.Y -= 250 / float64(ebiten.TPS())
+}
+
+func (l *Laser) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(l.position.X, l.position.Y)
+	screen.DrawImage(l.sprite, op)
+}
